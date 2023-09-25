081ae2d2b65a4dab2c91489c42ccbb396fb8818c
week9nd <maoxianfeng@gmail.com>
2023-09-25 22:16:43 +0900

    S08

--- sales/src/main/java/com/insurance/life/sales/common/command/CommandHandler.java
+++ sales/src/main/java/com/insurance/life/sales/common/command/CommandHandler.java
@@ -5,7 +5,7 @@ import java.lang.annotation.*;
 
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.METHOD, ElementType.CONSTRUCTOR, ElementType.ANNOTATION_TYPE})
+@Target({ElementType.METHOD})
 public @interface CommandHandler {
 
 }
